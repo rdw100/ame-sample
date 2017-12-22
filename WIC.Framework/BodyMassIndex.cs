b04ae94962f198ce3c3bf59676d2130d076a8ead
Ryan Dustin Wright <ryandustinwright@hotmail.com>
2017-12-22 13:32:15 -0500

    Story 00011: Added BMI utility class via VS 2017 ENT.

---
+++ WIC.Framework/BodyMassIndex.cs
@@ -0,0 +1,40 @@
+/// <summary>
+/// Calculates BMI (uses Pounds and inches)
+/// </summary>
+public class BodyMassIndex
+{
+    private double weight;
+    private double height;
+    private double bmi;
+    
+    /// <summary>
+    /// Computes a BMI value using weight and height params.
+    /// </summary>
+    /// <param name="initWeight"></param>
+    /// <param name="initHeight"></param>
+    public BodyMassIndex(double initWeight, double initHeight)
+    {
+        weight = initWeight;
+        height = initHeight;
+    }
+
+    /// <summary>
+    /// Gets the BMI value.
+    /// </summary>
+    /// <returns>Returns the calculated BMI.</returns>
+    public double getBMI()
+    {
+        return bmi;
+    }
+
+    /// <summary>
+    /// Calculates BMI.
+    /// </summary>
+    /// <remarks>
+    /// Formula: weight (lb) / [height (in)] ^ 2 x 703
+    /// </remarks>
+    public void CalculateBMI()
+    {
+        bmi = weight / (height ^ 2) * 703;
+    }
+}
